7837871de277fbe45ae39018fc138b2e11f9c9f9
Mohamed Maghzaoui <mohamed.maghzaoui53@gmail.com>
2023-08-30 16:35:00 +0200

    added some routes for homa and calendar

--- DeadlineApp/project/src/App.js
+++ DeadlineApp/project/src/App.js
@@ -1,10 +1,20 @@
 import { MyCalendar } from "./pages/calendar";
+import { Home } from "./pages/Home";
+import { Navbar } from "./pages/navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Calendar } from "react-big-calendar";
 
 function App() {
   return (
     <div>
-      <MyCalendar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<MyCalendar />} />
+          <Route path="*" element={<h1>page not available</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
